1046d37a1436d268cab8ad09682a28cb483fba83
Igor <ig0r260892@gmail.com>
2017-04-03 20:35:19 +0100

    Import spotify service in app component and declare it as provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SpotifyService } from './services/spotify.service';
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	styleUrls: ['./app.component.scss'],
+	providers: [ SpotifyService ]
 })
 export class AppComponent {
 	title = 'app works!';
