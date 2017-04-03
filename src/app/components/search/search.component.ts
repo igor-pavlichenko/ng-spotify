5ae3742d5d25ac7a3d4316b4a0f0df5b1f7f9ab9
Igor <ig0r260892@gmail.com>
2017-04-03 20:03:14 +0100

    Add search input which calls a search method on keyup

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+	searchStr: string;
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	searchMusic() {
+		console.log(this.searchStr);
 	}
 
 }
