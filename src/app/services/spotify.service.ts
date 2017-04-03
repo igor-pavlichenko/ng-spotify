73ed14d7f839161fe6dc8f3c28e27b58bf143ea2
Igor <ig0r260892@gmail.com>
2017-04-03 20:23:28 +0100

    Generate spotify service
    Add a searchMusic method that sends a get request with search query to the spotify api and returns result as an observable.

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SpotifyService {
+	private searchUrl: string;
+
+	constructor(private http: Http) {
+
+	}
+
+	searchMusic(str: string, type = 'artist') {
+		this.searchUrl = 'https://api.spotify.com/v1/search?query=' + str
+																	+ '&offset=0'
+																	+ '&limit=20'
+																	+ '&type=' + type
+																	+ '&market=US';
+
+		return this.http.get(this.searchUrl)
+						.map(result => result.json());
+	}
+
+}
