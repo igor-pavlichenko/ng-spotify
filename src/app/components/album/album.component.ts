abe0e0dc6fa9879dde1a19b71fcf82b2cf2c0309
Igor <ig0r260892@gmail.com>
2017-04-04 05:29:29 +0100

    Refine the album component
    Add required imports.
    Change selector.
    Add necessary properties and inject dependencies.
    Get the album on init.

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { SpotifyService } from '../../services/spotify.service';
+
+import { Artist } from '../../models/artist';
+import { Album } from '../../models/album';
 
 @Component({
-	selector: 'app-album',
+	selector: 'album',// tslint:disable-line
 	templateUrl: './album.component.html',
 	styleUrls: ['./album.component.scss']
 })
 export class AlbumComponent implements OnInit {
+	id: string;
+	album: Album;
 
-	constructor() { }
+	constructor(
+		private spotifyService: SpotifyService,
+		private route: ActivatedRoute
+	) { }
 
 	ngOnInit() {
+		this.route.params
+			.map(params => params['id']) // get current album's id from the url bar
+			.subscribe(id => {
+				// acquire the album
+				this.spotifyService.getAlbum(id)
+					.subscribe(album => {
+						this.album = album;
+					});
+
+			});
 	}
 
 }
